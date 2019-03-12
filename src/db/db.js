7804c736d3e910528f4866aae221c47cf6a861f8
Lucas O'Rourke <lor215@nyu.edu>
2019-03-11 20:42:16 -0400

    Added db get and delete functionality for Rooms

--- src/db/db.js
+++ src/db/db.js
@@ -11,4 +11,33 @@ var {
 	HistoryWorkers
 } = require('./sqlize');
 
+
+// gets a raw array of all rooms in the database
+router.get('/get/rooms', (req, res) => {
+	Rooms.findAll({ raw: true, order: [['rid', 'ASC']] }).then(rooms => {
+		res.json(rooms);
+	});
+});
+
+
+// creates new room
+router.post('/new/room', (req, res) => {
+	const { rid, rstatus } = req.body;
+
+	Rooms.create({ rid, rstatus }).then(r => {
+		res.json(r);
+	});
+});
+
+
+// deletes room
+router.post('/delete/room', (req, res) => {
+	const { rid } = req.body;
+
+	Rooms.findOne({ where: { rid } }).then(room => {
+		room.destroy().then(r => res.json(r));
+	});
+});
+
+
 module.exports = router;
